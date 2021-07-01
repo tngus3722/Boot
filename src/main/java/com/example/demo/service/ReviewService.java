4867aad52feea928194687a662b06789750f6684
DESKTOP-U31R4JK\user <tngu3722@koreatech.ac.kr>
2021-07-01 22:42:09 +0900

    test2

--- src/main/java/com/example/demo/service/ReviewService.java
+++ src/main/java/com/example/demo/service/ReviewService.java
@@ -3,7 +3,7 @@ package com.example.demo.service;
 import com.example.demo.domain.Review;
 
 import java.util.List;
-
+//test2
 public interface ReviewService {
     public List<Review> display(Integer fish_id);
     //test
